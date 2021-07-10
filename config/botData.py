b75e8fe4c07d84243b5728a47f7dee80e37026d3
maxtaran2010 <87073104+maxtaran2010@users.noreply.github.com>
2021-07-10 10:23:38 +0200

    token changed
    token changed

--- config/botData.py
+++ config/botData.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'ODU5MTE0MTExOTQwOTUyMTA1.YNn-XQ.s_sfXEme1aEupOjm62byfoQxeIA',
+    'token': 'ODU5MTE0MTExOTQwOTUyMTA1.YNn-XQ._RCl0Ap5slAt2cEXPqvmwsLBu34',
     'bot': 'blanker bot',
-    'id': 859114111940952105,
+    'id': 859114111940952105
     'prefix': '-'
 }
